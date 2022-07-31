725806dbc6854eab982f47573a584e158b2f6089
TuanVo3423 <tuanvanvo2003@gmail.com>
2022-07-31 10:53:39 +0700

    login gg

--- src/components/chatroom/component/Sidebar/component/UserInfo/index.js
+++ src/components/chatroom/component/Sidebar/component/UserInfo/index.js
@@ -8,19 +8,21 @@ import { AppContext } from '../../../../../../context/AppProvider';
 const cx = classNames.bind(styles);
 
 export default function UserInfo() {
-    const { clearState, selectedRoom } = useContext(AppContext);
+    const { clearState } = useContext(AppContext);
     const {
         user: { displayName, photoURL },
     } = useContext(AuthContext);
+    console.log('photoURL', photoURL);
     const handleSignOut = () => {
         clearState();
-        // console.log('selectedRoom', selectedRoom);
         auth.signOut();
     };
     return (
         <div className={cx('Info-wrapper')}>
             <div className={cx('left-info')}>
-                <Avatar src={photoURL}>{photoURL ? '' : displayName?.charAt(0)?.toUpperCase()}</Avatar>
+                <Avatar referrerPolicy="no-referrer" src={photoURL}>
+                    {photoURL ? '' : displayName?.charAt(0)?.toUpperCase()}
+                </Avatar>
                 <Typography.Text className={cx('user-name')}>{displayName}</Typography.Text>
             </div>
             <Button ghost onClick={handleSignOut}>
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -6,6 +6,7 @@ import classNames from 'classnames/bind';
 import styles from './login.module.scss';
 const cx = classNames.bind(styles);
 let fbProvider = new firebase.auth.FacebookAuthProvider();
+let ggProvider = new firebase.auth.GoogleAuthProvider();
 
 export default function Login() {
     const handleFbLogin = async () => {
@@ -23,13 +24,31 @@ export default function Login() {
             });
         }
     };
+    const handleggLogin = async () => {
+        const { additionalUserInfo, user } = await auth.signInWithPopup(ggProvider);
+        // check newbie to add info into the db
+        if (additionalUserInfo.isNewUser) {
+            console.log(user);
+            // collection , in firebase It means table in sql server
+            addDocument('users', {
+                displayName: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                uid: user.uid,
+                providerId: additionalUserInfo.providerId,
+                keywords: generateKeywords(user.displayName),
+            });
+        }
+    };
     return (
         <Row justify="center" style={{ height: 800 }}>
             <Col span={8}>
                 <Typography.Title level={3} className={cx('title')}>
                     Chào mừng đến với myChat
                 </Typography.Title>
-                <Button className={cx('btn-gg')}>Đăng nhập với Google</Button>
+                <Button onClick={handleggLogin} className={cx('btn-gg')}>
+                    Đăng nhập với Google
+                </Button>
                 <Button onClick={handleFbLogin} className={cx('btn-fb')}>
                     Đăng nhập với Facebook
                 </Button>
